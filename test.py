40cae7514d778fc99c051d0cd01e46b68e31e69a
Fernando <fepegar@gmail.com>
2020-05-15 19:54:18 +0100

    Fix test script

--- test.py
+++ test.py
@@ -3,20 +3,15 @@ import spotipy.util as util
 from spotipy.oauth2 import SpotifyClientCredentials
 
 credentials = SpotifyClientCredentials()
-# token = credentials.get_access_token()
 token = util.prompt_for_user_token(
-    username=username,
+    username='ferniss',
     scope='playlist-modify-private')
 spotify = spotipy.Spotify(auth=token)
 sp = spotify
 
-uri = 'spotify:user:ferniss:playlist:54OuamNpPqUoYzJJPgt1Nb'
+uri = 'spotify:playlist:0kNHFNoDBVu9u8SpwgWTos'
 username = 'ferniss'
 playlist = sp.user_playlist(username, uri)
 
-tempos_dict = {}
-for track_dict in playlist['tracks']['items']:
-    track = track_dict['track']
-    features = sp.audio_features(track['uri'])[0]
-    print(track['name'], features['tempo'])
-    tempos_dict[track['uri']] = features['tempo']
+for idx, track_dict in enumerate(playlist['tracks']['items']):
+    print(idx + 1, track_dict['track']['name'])
